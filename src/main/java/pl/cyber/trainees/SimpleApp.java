e990c708a289c0eb8fb939d228ca83d63a958a1c
Aleksandra Gowin <a.a.gowin@gmail.com>
2022-07-13 15:54:04 +0200

    dodawanie struktury Kalkulatora

--- src/main/java/pl/cyber/trainees/SimpleApp.java
+++ src/main/java/pl/cyber/trainees/SimpleApp.java
@@ -1,6 +1,7 @@
 package pl.cyber.trainees;
 
-import org.codehaus.groovy.transform.SourceURIASTTransformation;
+import pl.cyber.trainees.kalkulator.Dodawanie;
+import pl.cyber.trainees.kalkulator.Kalkulator;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
@@ -12,14 +13,24 @@ public class SimpleApp {
 
     public static String SEPARATOR_SPACJA = " ";
 
-
     public static void main(String[] args) {
-
         int scale = 0;
 
 
+    String imie = "Ola";
+    String miasto = "Krakow";
 
-//        String space = " ";
+
+    Dodawanie wynik;
+
+    Kalkulator.wynik(imie,miasto);
+
+
+
+
+
+
+//        Stringspace = " ";
 //        String a = "Ala";
 //        String b = "ma kota";
 //
@@ -32,19 +43,20 @@ public class SimpleApp {
         //Utworzenie 2 zmiennych typu String i ich inicjalizacja
         String a = "Ola";
         String b = "Gowin";
-        System.out.print(concat(a,b));
+        System.out.print("wynik concat: "+concat(a,b) );
 
 
         //Utworzenie 2 zmiennych typu Integer i ich inicjalizacja
-        Integer c = 1;
-        Integer d = 5;
-        System.out.print(c+d);
+        int c = 1;
+        int d = 5;
+        System.out.print(add(c,d));
 
 
         //Utworzenie 2 zmiennych typu Double i ich inicjalizacja
-        Double e = 2.5;
-        Double f = 1.5;
-        System.out.println(e-f);
+        double e = 2.5;
+        double f = 1.5;
+        System.out.println("wynik odejmowania Oli: " + odejmowanie(e,f) );
+
 
 
 
@@ -67,7 +79,7 @@ public class SimpleApp {
         roundNumber("2.6", scale);
         System.out.println("NEXT");
         //endregion
-
+/*
         //region praca domowa
         String valString1 = "pierwszy string";
         String valString2 = new String("drugi string"); // konstrukcja stowosana np czy odczycie informacji z pliku tekstowego zamiast napisu wtedy podaje się jakąś wartośc ze zmiennej
@@ -86,23 +98,32 @@ public class SimpleApp {
         BigDecimal valBigDec2 = new BigDecimal("4.5");
 
         System.out.println("dodawanie: " + add(valBigDec1, valBigDec2) + ", odejmowanie: " + subtract(valBigDec1, valBigDec2) + ", mnożenie: " + multiplication(valBigDec1, valBigDec2) + ", dzielenie: " + division(valBigDec1, valBigDec2));
-
+*/
 //        Problem dzielenia przez 0
 
 //        System.out.println("Dzielenie przez 0" +  division(valDouble1, 0.0));
 //        System.out.println("Dzielenie przez 0" +  division(valBigDec1, BigDecimal.ZERO));
 
 //Rozwiązanie:
+        Double valDouble1 = 10.5;
+        BigDecimal valBigDec1 = BigDecimal.valueOf(10.5);
         System.out.println("Dzielenie przez 0: " +  divisionByZero(valDouble1, 0.0));
         System.out.println("Dzielenie przez 0: " +  divisionByZero(valBigDec1, BigDecimal.ZERO));
         //endregion
 
     }
 
+    public static double odejmowanie(double l1,double l2){
+        var result = l1 - l2 ;
+        return result;
+    }
+
+
     public static int add(int l1, int l2) {
         var result = l1 + l2;
         return result;
     }
+
 
     public static String concat(String l1, String l2) {
         var result = l1 + l2 + "\n";
---
+++ src/main/java/pl/cyber/trainees/kalkulator/Kalkulator.java
@@ -0,0 +1,12 @@
+package pl.cyber.trainees.kalkulator;
+
+public class Kalkulator {
+
+    public static void wynik(){
+        System.out.println("To jest wynik");
+    }
+
+    public static void wynik(String imie, String miasto) {
+        System.out.println(imie + " " + miasto);
+    }
+}
