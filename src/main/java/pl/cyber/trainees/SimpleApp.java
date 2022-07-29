75f452d0ea7ccee81d18b82a805f7b70569f11c4
Aleksandra Gowin <a.a.gowin@gmail.com>
2022-07-29 20:06:12 +0200

    choinka

--- src/main/java/pl/cyber/trainees/SimpleApp.java
+++ src/main/java/pl/cyber/trainees/SimpleApp.java
@@ -1,7 +1,12 @@
 package pl.cyber.trainees;
 
+import pl.cyber.trainees.wyjasnienia.Petle;
+
 public class SimpleApp {
     public static void main(String[] args) {
+
+        Petle petle = new Petle();
+        petle.uruchom();
 
     }
 }
--- src/main/java/pl/cyber/trainees/wyjasnienia/Petle.java
+++ src/main/java/pl/cyber/trainees/wyjasnienia/Petle.java
@@ -1,5 +1,11 @@
 package pl.cyber.trainees.wyjasnienia;
 
+import pl.cyber.trainees.kalkulator.Dodawanie;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Petle {
 
    /*
@@ -45,4 +51,91 @@ for(int i = 0; i<5; i--); { -- tzw pętla nieskończona
  List<>
  remove(); metoda do usuwania rekordów
    */
-}
+
+    public void uruchom() {
+        List<BigDecimal> lista = new ArrayList<>();
+        lista.add(BigDecimal.valueOf(10.0));
+        lista.add(BigDecimal.valueOf(2.0));
+        lista.add(BigDecimal.valueOf(5.6));
+        lista.add(BigDecimal.valueOf(21.8));
+        lista.add(BigDecimal.valueOf(2.1));
+        lista.add(BigDecimal.valueOf(41.0));
+        lista.add(BigDecimal.valueOf(14.0));
+        lista.add(BigDecimal.valueOf(1.0));
+        lista.add(BigDecimal.valueOf(4.0));
+
+        rozwiazanieZadanie1(lista);
+        choinkaRozbudowana(4);
+    }
+
+    //  Zadanie nr 1
+//  Wykonać metodę która przyjmie jako parametr List<BigDecimal>, następnie wykona następujące zagadnienia:
+//  a) wyświetli każdy element listy jeden po drugim
+//  b) każdy element listy doda do siebie.
+
+    private void rozwiazanieZadanie1(List<BigDecimal> list) {
+//            BigDecimal sumaLiczb = BigDecimal.ZERO;
+            BigDecimal sumaLiczb = BigDecimal.valueOf(0);
+
+
+        for (BigDecimal liczba :
+                list) {
+
+            System.out.println(liczba);
+
+            sumaLiczb = sumaLiczb.add(liczba);
+
+        }
+
+                System.out.println("Suma liczb: " + sumaLiczb);
+            }
+
+        //Zadanie Choinka
+        //klasyczne fory (a dokladnie 2 fory)
+        //
+        //metoda bedzie przyjmowac parametr jako wysokosc
+
+        private void choinkaProsta() {
+            System.out.println("  *");
+            System.out.println(" ***");
+            System.out.println(" *****");
+        }
+
+    private void choinkaRozbudowana(Integer wysokoscChoinki) {
+
+        Integer iloscGwiazdekWPodstawie = (wysokoscChoinki *2) - 1;
+
+        for(int wysokosc = 1; wysokosc < wysokoscChoinki; wysokosc++) {
+            for (int szerokosc = 1; szerokosc < iloscGwiazdekWPodstawie; szerokosc++) {
+                if (szerokosc < wysokosc) {
+                    System.out.println(" ");
+                } else {
+                    System.out.println("*");
+                }
+            }
+                System.out.println();
+            }
+        }
+
+//        for (int wysokosc = 0; wysokosc < wysokoscChoinki; wysokosc++) {
+//            for (int szerokosc = 0; szerokosc < wysokoscChoinki*2; szerokosc++) {
+//                if(szerokosc < (wysokoscChoinki - wysokosc) || szerokosc > (wysokoscChoinki + wysokosc)) {
+//                    System.out.print(" ");
+//                } else {
+//                    System.out.print("*");
+//                }
+//            }
+//            System.out.println();
+
+
+
+    }
+
+
+
+
+
+
+
+
+
