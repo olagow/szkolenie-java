b8b2fe35d10b174475f96a581dcf2bcb69db46fa
Aleksandra Gowin <a.a.gowin@gmail.com>
2022-07-29 17:01:56 +0200

    utworzenie brancha Spotkanie30tydz

--- src/main/java/pl/cyber/trainees/dziedziczenie/Kolor.java
+++ src/main/java/pl/cyber/trainees/dziedziczenie/Kolor.java
@@ -1,9 +1,9 @@
-package pl.cyber.trainees.dziedziczenie;
-
-
-//enum-typ wyliczeniowy
-public enum Kolor {
-    CZERWONY,//to jest kolor czerwony jakiegos pojazdu
-    ZIELONY,
-    NIEBIESKI
-}
+//package pl.cyber.trainees.dziedziczenie;
+//
+//
+////enum-typ wyliczeniowy
+//public enum Kolor {
+//    CZERWONY,//to jest kolor czerwony jakiegos pojazdu
+//    ZIELONY,
+//    NIEBIESKI
+//}
